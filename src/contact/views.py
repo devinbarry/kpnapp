238d6c077257905e041c788233cb658b2ca85fcf
Devin Barry <devinbarry@gmail.com>
2016-03-01 14:50:05 +0100

    Add DjangoRestFramework and expose the Contact object via a REST API.

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ContactSerializer
+from .models import Contact
 
-# Create your views here.
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
