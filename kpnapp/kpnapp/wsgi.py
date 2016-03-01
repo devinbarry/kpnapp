997aa3c8c953cca5e806236ddb42e153107dfd4a
Devin Barry <devinbarry@gmail.com>
2016-03-01 14:05:23 +0100

    Initial project files commit.

---
+++ kpnapp/kpnapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kpnapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kpnapp.settings")
+
+application = get_wsgi_application()
